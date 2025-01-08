71a8484d187d9425b30219b280b6037d8b73c72c
sxy8469 <42426110+sxy8469@users.noreply.github.com>
2025-01-08 18:26:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const config = {
-    define: {
-      "process.env.NODE_ENV": JSON.stringify(mode),
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    plugins: [react()],
-    build: {
-      sourcemap: false,
-    },
-  };
-  return config;
+    const config = {
+        define: {
+            "process.env.NODE_ENV": JSON.stringify(mode),
+        },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+                "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+            },
+        },
+        plugins: [react()],
+        build: {
+            sourcemap: false,
+        },
+    };
+    return config;
 });
